0c9c2f83f57eae39b4b94c34f772beb4cda5f667
Amit <maheshwarvaranasi@gmail.com>
2021-06-05 21:05:01 +0530

    added login and sign up functionality in the app using firebase

---
+++ src/Components/Login/index.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from "react-router";
+import "./Login.css"
+import fire from '../../firebase';
+const Login = () => {
+    const [loginS,setLoginS] = useState(false);
+    const [username,_setUserName] = useState("");
+    const [email,_setEmail] = useState("");
+    const [password,_setPassword] = useState("");
+    const [emailError,_setEmailError] = useState("");
+    const [passwordError,_setPasswordError] = useState("");
+    const [hasAccount,_setHasAccount] = useState(false);
+    const [isLogin,_setUserLogin]  = useState(false);
+    const history = useHistory();
+
+    const LoginFunc = () => {
+        // history.push("/dashboard");
+        // setloggedin(true);
+    }
+
+    const _clearInputs = ()=>{
+        _setEmail("");
+        _setPassword("");
+    }
+
+    const _clearErrors = ()=>{
+        _setEmailError("");
+        _setPasswordError("");
+    }
+
+    const _handleLogin = ()=>{
+        _clearErrors();
+        fire
+            .auth()
+            .signInWithEmailAndPassword(email,password)
+            .catch((err)=>{
+                switch(err.code) {
+                    case "auth/invalid-email":
+                    case "auth/user-disabled":
+                    case "auth/user-not-found":
+                        _setEmailError(err.message);
+                        break;
+                    case "auth/wrong-password":
+                         _setPasswordError(err.message);
+                        break;
+
+                }
+            })
+    }
+
+    const _handleSignup = ()=>{
+        _clearErrors()
+        fire
+            .auth()
+            .createUserWithEmailAndPassword(email,password)
+            .catch((err)=>{
+                switch(err.code) {
+                    case "auth/email-already-in-use":
+                    case "auth/invalid-email":
+                        _setEmailError(err.message);
+                        break;
+                    case "auth/weak-password":
+                         _setPasswordError(err.message);
+                        break;
+
+                }
+            })
+    }
+
+    const _handleLogout = ()=>{
+        fire.auth().signOut();
+    }
+
+    const authListener = ()=>{
+        fire.auth().onAuthStateChanged((user)=>{
+            if(user){
+                _clearInputs()
+                _setUserName(user);
+            }else{
+                _setUserName("");
+            }
+        })
+    }
+    
+    useEffect(()=>{
+        authListener();
+    },[])
+
+    return (
+        <div className="overlay">
+            <div className="component">
+                <div className="body-component">
+                    <div className="header">
+                    {isLogin===false?"Login":"Signup"}
+                    </div>
+                    <div className="mobile-header">Email Id</div>
+                    <input type="text" className="mobile-number-box" value={email} onChange={(e)=>{_setEmail(e.target.value)}}/>
+                    {emailError.length>0&&<div className="error">{emailError}</div>}
+                    <div className="mobile-header">Password:</div>
+                    <input type="password" className="mobile-number-box" value={password} onChange={(e)=>{_setPassword(e.target.value)}}/>
+                    {passwordError.length>0&&<div className="error">{passwordError}</div>}
+                    <br/>
+                    <input type="button" value="Log In" className="login-button-div" onClick={isLogin===false?_handleLogin:_handleSignup}/>
+                    {isLogin===false&&
+                    <div className="suggest-signup-div">if you dont have an account please 
+                    <span className="sign-up-span" 
+                        onClick={()=>{_setUserLogin(true)}}>
+                    sign up here</span></div>}
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Login;
